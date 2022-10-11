603abe776f1887dfb622539f0cbfd783795ca4bc
JosephMSU <jlbaker5@moreheadstate.edu>
2022-10-11 18:39:32 -0400

    f

---
+++ New/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 5f;
+    bool left = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal > 0)
+        {
+            left = false;
+        }
+        else if (horizontal < 0)
+        {
+            left = true;
+
+        }
+
+        Vector3 pos = transform.position;
+        pos.x += (horizontal * Time.deltaTime * speed);
+
+
+    }
+}
