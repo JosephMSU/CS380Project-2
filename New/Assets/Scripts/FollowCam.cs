ad1f36778ac93800cf615e6d7ced54f636391e7b
kfjasonkf <kfjasonkf@gmail.com>
2022-10-11 22:45:15 -0400

    a

---
+++ New/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cam;
+    [SerializeField]
+    private GameObject _hero;
+    [SerializeField]
+    private float _zOffset;
+
+    private Vector3 _camPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camPos = _hero.transform.position;
+        _camPos.z -= _zOffset;
+        _cam.transform.position = _camPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 _heroPos = _hero.transform.position;
+        _camPos.x = _heroPos.x;
+        _camPos.y = _heroPos.y;
+        _cam.transform.position = _camPos;
+    }
+}
