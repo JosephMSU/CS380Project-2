3b432f1f8a1beb5bf4dbaa2bea319f47e7468f22
kfjasonkf <kfjasonkf@gmail.com>
2022-10-12 22:44:30 -0400

    Created an Enemy script
    Also added a few things to the Player script to interface with it.

--- New/Assets/Scripts/Player.cs
+++ New/Assets/Scripts/Player.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Added for interfacing with the enemy script. Feel free to change the name or
+    // suggest a different method.  - Jason
+    [HideInInspector]
+    public static bool invincible;
+
+    [SerializeField]
     float speed = 5f;
+
     bool left = false;
     // Start is called before the first frame update
     void Start()
@@ -30,6 +37,15 @@ public class Player : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += (horizontal * Time.deltaTime * speed);
 
+
+    }
+
+    // The player takes damage (added to interface with Enemy class.  I wanted to make it possible for
+    // different enemys to give the player different amounts of damage if we decide to do so, and wanted to
+    // make sure you are aware of it by adding this.  Feel free to change the name of the function, and I will
+    // update the Enemy script to match, or you can suggest a different approach.)  -Jason 
+    public void TakeDamage(int dmgAmt)
+    {
 
     }
 }
