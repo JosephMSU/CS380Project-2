3c32c24d2c8976b25c2c3fd3c92e1415a1dc7175
kfjasonkf <kfjasonkf@gmail.com>
2022-10-12 21:52:12 -0400

    Update camera script

--- New/Assets/Scripts/FollowCam.cs
+++ New/Assets/Scripts/FollowCam.cs
@@ -7,25 +7,39 @@ public class FollowCam : MonoBehaviour
     [SerializeField]
     private GameObject _hero;
     [SerializeField]
+    private float _xOffset = 0;
+    [SerializeField]
+    private float _yOffset = 2.5f;
+    [SerializeField]
     private float _zOffset = 10;
 
+
     private Vector3 _camPos;
+    private Vector3 _oldPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
         _camPos = _hero.transform.position;
+        _camPos.x += _xOffset;
+        _camPos.y += _yOffset;
         _camPos.z -= _zOffset;
         transform.position = _camPos;
+        _oldPos = _camPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 _heroPos = _hero.transform.position;
-        _camPos.x = _heroPos.x;
-        _camPos.y = _heroPos.y;
+        Vector3 heroPos = _hero.transform.position;
+        Vector3 newPos;
+        newPos.x = heroPos.x + _xOffset;
+        newPos.y = heroPos.y + _yOffset;
+        newPos.z = -(_zOffset);
+
+        _camPos = Vector3.Lerp(_oldPos, newPos, 0.05f);
+        _oldPos = _camPos;
         transform.position = _camPos;
     }
 }
