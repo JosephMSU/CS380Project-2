171586226cb2d296ae30d9f0f499746d64d88746
JosephMSU <jlbaker5@moreheadstate.edu>
2022-10-25 09:50:58 -0400

    Update Player.cs

--- New/Assets/Scripts/Player.cs
+++ New/Assets/Scripts/Player.cs
@@ -30,9 +30,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        /*Vector3 down = transform.TransformDirection(Vector3.down) * 10;
-        Debug.DrawRay(transform.position, down, Color.green);*/
+        /*float x = this.transform.position.x - .4f;
+        float x2 = this.transform.position.x + .4f;
+        Vector3 position4 = new Vector3(x, this.transform.position.y, this.transform.position.z);
+        Vector3 position5 = new Vector3(x2, this.transform.position.y, this.transform.position.z);
+        Vector3 down = transform.TransformDirection(Vector3.down) * 10;
+        Debug.DrawRay(transform.position, down, Color.green);
+        Debug.DrawRay(position4, down, Color.red);
+        Debug.DrawRay(position5, down, Color.blue);*/
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         if (horizontal > 0)
@@ -46,10 +51,11 @@ public class Player : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            if (isGrounded())
-            {
-                rBody.velocity = Vector3.up * jumpSpeed;
-            }
+            shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump();
         }
 
         Vector3 pos = transform.position;
@@ -71,17 +77,35 @@ public class Player : MonoBehaviour
 
     public void jump()
     {
-        
+        if (isGrounded())
+        {
+            rBody.velocity = Vector3.up * jumpSpeed;
+        }
+    }
+
+    public void shoot()
+    {
+
     }
 
     public bool isGrounded()
     {
-        RaycastHit hit;
-        Ray ray = new Ray(this.transform.position, (this.transform.up * -1));
-        Physics.Raycast(ray, out hit);
-        Vector3 hitPosition = hit.point;
-        float hitDistance = hit.distance;
-        Debug.Log(hitDistance);
-        return (hitDistance == 0);
+        RaycastHit hit1;
+        Ray ray1 = new Ray(this.transform.position, (this.transform.up * -1));
+        Vector3 position2 = new Vector3(this.transform.position.x - .4f, this.transform.position.y, this.transform.position.z);
+        Vector3 position3 = new Vector3(this.transform.position.x + .4f, this.transform.position.y, this.transform.position.z);
+        RaycastHit hit2;
+        Ray ray2 = new Ray(this.transform.position, (this.transform.up * -1));
+        RaycastHit hit3;
+        Ray ray3 = new Ray(this.transform.position, (this.transform.up * -1));
+        Physics.Raycast(ray2, out hit2);
+        Physics.Raycast(ray3, out hit3);
+        Physics.Raycast(ray1, out hit1);
+        Vector3 hitPosition = hit1.point;
+        float hitDistance1 = hit1.distance;
+        float hitDistance2 = hit2.distance;
+        float hitDistance3 = hit3.distance;
+        Debug.Log(hitDistance1);
+        return (hitDistance1 == 0 || hitDistance2 == 0 || hitDistance3 == 0);
     }
 }
